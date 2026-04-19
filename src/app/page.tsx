715e8daa8cab7f41b637cbeba330c6221f7469ab
Your Name <niyhyhiy@gmail.com>
2026-04-19 17:57:42 -0400

    a test for vercel

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,10 @@ export default function Home() {
   return (
     <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <h1>Welcome to Juan French</h1>
+      <p className="text-gray-500 dark:text-gray-400">
+        This is a starter template for using Clerk with Next.js 13 and the new
+        app directory.
+      </p>
     </div>
   );
 }
