713794b756f75e8576d7ed3bda282e2b958142b1
Your Name <niyhyhiy@gmail.com>
2026-04-20 08:42:16 -0400

    add Sign In and Sign Up button, add dark mode

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,13 @@
+import ModeToggle from '@/components/ModeToogle';
+import { Button } from '@/components/ui/button';
 import Image from 'next/image';
 
 export default function Home() {
   return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <h1>Welcome to Juan French</h1>
-      <p className="text-gray-500 dark:text-gray-400">
-        This is a starter template for using Clerk with Next.js 13 and the new
-        app directory.
-      </p>
+    <div>
+      <Button>Sign In</Button>
+      <Button variant="outline">Sign Up</Button>
+      <ModeToggle />
     </div>
   );
 }
